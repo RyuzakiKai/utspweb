b23c5ed13b3587436826af9f22767944b503fb70
Arfan Muhtada <129240607+RyuzakiKai@users.noreply.github.com>
2023-05-17 00:21:20 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class = image-logo-center>
       <img src="../assets/LOGO.png" width="300" alt="logo"/>
+      <h3>“Siapa saja membaca satu huruf dari Kitabullah (Alquran) maka dia akan mendapat satu kebaikan. Sedangkan satu kebaikan dilipatkan kepada sepuluh semisalnya. Aku tidak mengatakan alif lâm mîm satu huruf. Akan tetapi, alif satu huruf, lâm satu huruf, dan mîm satu huruf.” (HR At-Tirmidzi).</h3>
   </div>
 </template>
 
